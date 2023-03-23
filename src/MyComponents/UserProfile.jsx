c9ba6faf4b8106f2421ed97e9980ff3501fad444
HemantShankar <hemantshankar9595@gmail.com>
2023-03-23 17:39:32 +0530

    updated user profile page

--- src/MyComponents/UserProfile.jsx
+++ src/MyComponents/UserProfile.jsx
@@ -9,14 +9,40 @@ const UserProfile = () => {
             </div>
             <div className="cover">
                 <div className="info">
-                    <div className='profile-photo'>
-                        <img id='dp' src={Bg_image} alt="Your Picture" />
+                    <div className="vertical-1">
+                        <div className='profile-photo'>
+                            <img id='dp' src={Bg_image} alt="Your Picture" />
+                        </div>
+                        <div className="welcome-text">
+                            <h1 id='text'>Welcome, Hemant</h1>
+                            <p id='submit-num'>05 Submissions</p>
+                        </div>
                     </div>
-                    <div className="welcome-text">
-                        <h1 id='text'>Welcome, Hemant</h1>
-                        <p id='submit-num'>05 Submissions</p>
+
+                    <div className="personal-info">
+                        <div className="name" id='segment'>
+                            <div className="tag">Name:</div>
+                            <div className="tag-ans">Hemant Shankar</div>
+                        </div>
+                        <div className="email" id='segment'>
+                            <div className="tag">E-mail:</div>
+                            <div className="tag-ans">hemantshankar9595@gmail.com</div>
+                        </div>
+                        <div className="occu" id='segment'>
+                            <div className="tag">Occupation:</div>
+                            <div className="tag-ans">Working Professional</div>
+                        </div>
+                        <div className="comp" id='segment'>
+                            <div className="tag">Company:</div>
+                            <div className="tag-ans">E-Cell</div>
+                        </div>
+                        <div className="batch" id='segment'>
+                            <div className="tag">Batch:</div>
+                            <div className="tag-ans">2020-2025</div>
+                        </div>
                     </div>
-                    
+
+                    <button type="input" className="btn btn-primary text-center" id='edit_button'>Edit</button>
                 </div>
 
                 <div className="submissions">
