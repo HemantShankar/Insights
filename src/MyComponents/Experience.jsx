cece98b06feba89d88114f5246fd7dc0bad21794
HemantShankar <hemantshankar9595@gmail.com>
2023-03-27 20:13:40 +0530

    updated experience

--- src/MyComponents/Experience.jsx
+++ src/MyComponents/Experience.jsx
@@ -5,38 +5,80 @@ const Experience = () => {
   return (
     <div className="page">
       <div className="exp-heading">
-        <h1>Your Interview Experience</h1>
+        <h2>Interview Experience</h2>
       </div>
-      <form className="row g-3" id='form_signup'>
+
+      <form action="action.php">
+        <div className='hor-inp'>
+          <div className="half">
+            <h4>Company Name: </h4>
+            <input type="text" className="input input-name" aria-label="Company name" />
+          </div>
+          <div className="other-half">
+            <h4>Role: </h4>
+            <input type="text" className="input input-role" aria-label="Role" />
+          </div>
+        </div>
+
+        <div className='hor-inp'>
+          <div className="half">
+            <h4>Difficulty: </h4>
+            <input className="form-check input-1" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+            <label className="form-check-label" for="flexRadioDefault2">
+              Easy
+            </label>
+            <input className="form-check input-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <label className="form-check-label" for="flexRadioDefault1">
+              Medium
+            </label>
+            <input className="form-check input-3" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <label className="form-check-label" for="flexRadioDefault1">
+              Hard
+            </label>
+
+          </div>
+          <div className="other-half">
+            <h4>Number of Rounds: </h4>
+            <input type="text" className="input input-rounds" aria-label="Company name" />
+          </div>
+        </div>
+      </form>
+
+      {/* <form className="row g-3" id='form_signup'>
         <div className="row g-3">
           <div className="col">
             <label htmlFor="inputfirstname" className="form-label">Company Name</label>
-            <input type="text" className="form-control" aria-label="Company name" />
+            <input type="text" className="form-control mr-3" aria-label="Company name" />
           </div>
           <div className="col">
             <label htmlFor="inputsurname" className="form-label">Role</label>
             <input type="text" className="form-control" aria-label="Role" />
           </div>
         </div>
+
+        <div className="form-check col">
+          <label htmlFor="inputprofession" className="form-label">Difficulty :</label>
+          <input className="form-check-input-1 mx-3" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+          <label className="form-check-label" for="flexRadioDefault2">
+            Easy
+          </label>
+          <input className="form-check-input-2 mx-3" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+          <label className="form-check-label" for="flexRadioDefault1">
+            Medium
+          </label>
+          <input className="form-check-input-2 mx-3" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+          <label className="form-check-label" for="flexRadioDefault1">
+            Hard
+          </label>
+        </div>
+
         <div className="col-md-6">
-          <label htmlFor="inputEmail4" className="form-label">Email</label>
-          <input type="email" className="form-control" id="inputEmail4" />
+          <label htmlFor="inputEmail4" className="form-label row">Email</label>
+          <input type="email" className="form-control row" id="inputEmail4" />
         </div>
         <div className="col-md-6">
           <label htmlFor="inputPassword4" className="form-label">Password</label>
           <input type="password" className="form-control" id="inputPassword4" />
-        </div>
-
-        <div className="form-check">
-          <label htmlFor="inputprofession" className="form-label">You are a :</label>
-          <input className="form-check-input-1 mx-3" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
-          <label className="form-check-label" for="flexRadioDefault2">
-            Student
-          </label>
-          <input className="form-check-input-2 mx-3" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-          <label className="form-check-label" for="flexRadioDefault1">
-            Working Professional
-          </label>
         </div>
 
         <div className="col-12">
@@ -46,7 +88,7 @@ const Experience = () => {
         <div className="col-12" >
           <button type="submit" className="btn btn-primary" id='submit_button'>Create Account &#8702;</button>
         </div>
-      </form>
+      </form> */}
     </div>
 
 
