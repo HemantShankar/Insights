b7c229290b3293cace17052f78f46cf48d509502
HemantShankar <hemantshankar9595@gmail.com>
2023-04-02 19:53:17 +0530

    added images and created right part of home page

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     {/* <Experience/> */}
     {/* <About/> */}
     <Home/>
-    {/* <Footer/> */}
+    <Footer/>
     </>
     
   );
--- src/MyComponents/Home.jsx
+++ src/MyComponents/Home.jsx
@@ -1,17 +1,92 @@
 import React from 'react'
+import Bg_image1 from "./assests/google-logo.jpg"
+import Bg_image2 from "./assests/warner-music.png"
+import Bg_image3 from "./assests/Fb.jpg"
+import Bg_image4 from "./assests/vaseline.jpg"
+import Bg_image5  from "./assests/walmart.jpg"
+import Bg_image6 from "./assests/volkswagen.jpg"
 import './Home.css'
 
 const Home = () => {
-  return (
-    <div className='Home-page'>
-        <div className="first-half">
-            <h1>tHis is 1st half</h1>
+    return (
+        <div className='Home-page'>
+            <div className="first-half">
+                <h1>tHis is 1st half</h1>
+            </div>
+
+
+            <div className="second-half">
+                <h2 className='sec-head'>Interesting Facts</h2>
+                <div className="hor-fact">
+                    <div className="fact fact-1">
+                        <img className='fact-img' src={Bg_image1} alt="Your Picture" />
+                        <h4 className='fact-head'>Google</h4>
+                        <p className="fact-para">
+                            It is not common knowledge but Google was originally named BackRub. Larry Page and Sergey Brin renamed their company ‘Google’ in 1997.
+                        </p>
+                    </div>
+                    <div className="fact fact-2">
+                        <img className='fact-img' src={Bg_image2} alt="Your Picture" />
+                        <h4 className='fact-head'>Warner Music</h4>
+                        <p className="fact-para">
+                            For a long time, Warner Music held the copyrights to “Happy Birthday”. This meant that the company could collect royalties from anyone who ever sang it
+                        </p>
+                    </div>
+                </div>
+
+                <div className="hor-fact">
+                    <div className="fact fact-3">
+                        <img className='fact-img' src={Bg_image3} alt="Your Picture" />
+                        <h4 className='fact-head'>Facebook</h4>
+                        <p className="fact-para">
+                            Facebook is primarily blue in colour because its owner and chief, Mark Zuckerberg suffers from red-green colour blindness. Blue is the only colour he sees best.
+                        </p>
+                    </div>
+                    <div className="fact fact-4">
+                        <img className='fact-img' src={Bg_image4} alt="Your Picture" />
+                        <h4 className='fact-head'>Vaseline</h4>
+                        <p className="fact-para">
+                            Founder of Vaseline ate a spoonful of the product every day. It was his unfaltering belief in his product that birthed this habit which continued until his death at the age of 96.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="hor-fact">
+                    <div className="fact fact-5">
+                        <img className='fact-img' src={Bg_image5} alt="Your Picture" />
+                        <h4 className='fact-head'>Walmart</h4>
+                        <p className="fact-para">
+                            Every week, without fail, nearly one-third of the US’s population visits the Walmart stores. That’s about 100 million customers visiting Walmart to satisfy their shopping needs.
+                        </p>
+                    </div>
+                    <div className="fact fact-6">
+                        <img className='fact-img' src={Bg_image6} alt="Your Picture" />
+                        <h4 className='fact-head'>Volkswagen</h4>
+                        <p className="fact-para">
+                            Volkswagen Group owns some of the world’s biggest auto brands. The groups sells Audi, Bentley, Bugatti, Lamborghini, Porsche, SEAT, and Škoda. It also sells motorcycles under the Ducati brand.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="hor-fact">
+                    <div className="fact fact-5">
+                        <img className='fact-img' src={Bg_image5} alt="Your Picture" />
+                        <h4 className='fact-head'>Coca-Cola</h4>
+                        <p className="fact-para">
+                        Coca-Cola has around 3500 beverages and 500 products under its catalog.
+                        </p>
+                    </div>
+                    <div className="fact fact-6">
+                        <img className='fact-img' src={Bg_image6} alt="Your Picture" />
+                        <h4 className='fact-head'>Volkswagen</h4>
+                        <p className="fact-para">
+                            Volkswagen Group owns some of the world’s biggest auto brands. The groups sells Audi, Bentley, Bugatti, Lamborghini, Porsche, SEAT, and Škoda. It also sells motorcycles under the Ducati brand.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="second-half">
-            <h1>this is 2nd half</h1>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Home
